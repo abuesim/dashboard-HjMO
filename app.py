1e8e5f0e554475972d8bafdd7dedc22f37880b9c
abuesim <abu.esim@gmail.com>
2025-06-01 17:11:44 +0300

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# ----- تاغات منع الكاش -----
+st.markdown("""
+    <!-- منع المتصفح من تخزين الكاش -->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+""", unsafe_allow_html=True)
+
 st.set_page_config(page_title="لوحة التحكم المتقدمة", layout="wide")
 
 # ----- شريط علوي -----
